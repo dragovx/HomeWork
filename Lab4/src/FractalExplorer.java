2bcef68861b19d22617a72a6569c92e0467e9fba
dragovx <dragovx@mail.ru>
2020-03-02 02:19:12 +0300

    lab4(2)

--- Lab4/src/FractalExplorer.java
+++ Lab4/src/FractalExplorer.java
@@ -12,14 +12,14 @@ public class FractalExplorer {
     public FractalExplorer(int size) {
         this.size = size;
         fgen.getInitialRange(range);
-
-
     }
 
     public void createAndShowGUI() {
         JFrame jfrm = new JFrame("Fractal");
         JButton jbt = new JButton("Reset");
+        JButton jbt1 = new JButton("Zoom");
         jfrm.add(jbt, BorderLayout.SOUTH);
+        jfrm.add(jbt1, BorderLayout.NORTH);
         jimage = new JImageDisplay(size,size);
         jfrm.add(jimage, BorderLayout.CENTER);
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,8 +27,12 @@ public class FractalExplorer {
         jfrm.setVisible(true);
         jfrm.setResizable(false);
         jbt.addActionListener(new TestActionListener());
-
-
+        jbt1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                fgen.recenterAndZoomRange(range,0.1,0.1,0.9);
+                drawFractal();
+            }
+        });
     }
 
     private void drawFractal() {
@@ -52,25 +56,30 @@ public class FractalExplorer {
 
     public class TestActionListener implements ActionListener {
         public void actionPerformed(ActionEvent ae){
-            jimage.clearImage();
+            fgen.getInitialRange(range);
             drawFractal();
         }
     }
 
     public class TestMouseListener implements MouseListener {
         public void mouseClicked(MouseEvent mouseEvent) {
-            fgen.recenterAndZoomRange(range,25,25,0.5);
+            double mouseX = MouseInfo.getPointerInfo().getLocation().getX();
+            double mouseY = MouseInfo.getPointerInfo().getLocation().getY();
+            fgen.recenterAndZoomRange(range,mouseX,mouseY,0.5);
         }
         public void mousePressed(MouseEvent mouseEvent) { }
         public void mouseReleased(MouseEvent mouseEvent) { }
         public void mouseEntered(MouseEvent mouseEvent) { }
         public void mouseExited(MouseEvent mouseEvent) { }
+
     }
 
     public static void main(String[] args){
         FractalExplorer f = new FractalExplorer(800);
         f.createAndShowGUI();
         f.drawFractal();
+
     }
+
 }
 
