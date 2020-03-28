9d32257ec6cda2955cd2d89e58420e14768b828f
dragovx <dragovx@mail.ru>
2020-03-28 09:55:27 +0300

    Lab 7(3)

--- LAB7 WEB scan/src/Crawler.java
+++ LAB7 WEB scan/src/Crawler.java
@@ -4,17 +4,18 @@ import java.util.*;
 
 public class Crawler {
 
-    private int max_depth = 10;
-    public static final String URL_PREFIX = "http://";
+    private int max_depth = 2;
+    public static final String URL_PREFIX = "https://";
 
     Crawler(String host) throws IOException {
         Socket soc = new Socket(host, 80);
-        URLDepthPair hm = new URLDepthPair(URL_PREFIX + host);
+        URLDepthPair hm = new URLDepthPair("https://"+ host);
+        String host1 = host;
         LinkedList<URLDepthPair> viewed_url = new LinkedList<>();
         LinkedList<URLDepthPair> not_viewed_url = new LinkedList<>();
         not_viewed_url.add(hm);
         soc.setSoTimeout(2000);
-        while ((not_viewed_url.getFirst()!=null)) {
+        while ((!not_viewed_url.isEmpty())) {
             try {
                 URL url = new URL(not_viewed_url.getFirst().getURLS());
                 try {
@@ -25,7 +26,7 @@ public class Crawler {
                             try {
                                 if (string.contains("href=\"" + URL_PREFIX) & reta.startsWith(URL_PREFIX)) {
                                     hm = new URLDepthPair(reta.substring(0, reta.indexOf("\"")), (reta.substring(0, reta.indexOf("\"")).split("/").length - 3));
-                                    if (!not_viewed_url.contains(hm)) {
+                                    if (!not_viewed_url.contains(hm) & !viewed_url.contains(hm) & hm.getDepth()<max_depth & hm.getURLS().contains(host1)) {
                                         not_viewed_url.add(hm);
                                     }
                                     System.out.println(reta.substring(0, reta.indexOf("\"")));
@@ -53,12 +54,21 @@ public class Crawler {
                 }
             });*/
             viewed_url.add(not_viewed_url.getFirst());
-            not_viewed_url.getFirst();
+            not_viewed_url.removeFirst();
+            System.out.println(not_viewed_url);
+            System.out.println("234");
+            System.out.println(viewed_url);
         }
+        viewed_url.sort(new Comparator<URLDepthPair>() {
+            @Override
+            public int compare(URLDepthPair o1, URLDepthPair o2) {
+                return o1.getURLS().compareTo(o2.getURLS());
+            }
+        });
         System.out.println(viewed_url);
     }
 
     public static void main(String args[]) throws Exception {
-        new Crawler("google.com");
+        new Crawler("vk.com");
     }
 }
---
+++ LAB7 WEB scan/src/asdas.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class asdas {
+    public static void main(String args[]){
+        String a = "google.com";
+        String a1 = "goog";
+        System.out.println(a.contains(a1));
+    }
+}
