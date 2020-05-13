c9d5a6e21ce020ceb511687c9872f37248287986
dragovx <dragovx@mail.ru>
2020-05-13 13:17:02 +0300

    Lab 7(+)

--- LAB7 WEB scan/src/Crawler.java
+++ LAB7 WEB scan/src/Crawler.java
@@ -1,88 +1,75 @@
-import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.io.*;
 
 public class Crawler {
 
-    private int max_depth = 1;
-    public static final String URL_PREFIX = "https://";
+    public static final String URL_PREFIX="http://";
 
-    Crawler(String host) throws IOException {
-        Socket soc = new Socket(host, 80);
-        URLDepthPair hm = new URLDepthPair("https://"+ host + "/");
-        String host1 = host;
-        LinkedList<URLDepthPair> viewed_url = new LinkedList<>();
-        LinkedList<URLDepthPair> not_viewed_url = new LinkedList<>();
-        not_viewed_url.add(hm);
-        soc.setSoTimeout(2000);
-        while ((!not_viewed_url.isEmpty())) {
-            try {
-                URL url = new URL(not_viewed_url.getFirst().getURLS());
-                try {
-                    LineNumberReader reader = new LineNumberReader(new InputStreamReader(url.openStream()));
-                    String string = reader.readLine();
-                    while (string != null) {
-                        for (String reta : string.split("href=\""))
-                            try {
-                                if (string.contains("href=\"" + URL_PREFIX) & reta.startsWith(URL_PREFIX)) {
-                                    hm = new URLDepthPair(reta.substring(0, reta.indexOf("\"")), (reta.substring(0, reta.indexOf("\"")).split("/").length - 3));
-                                    if (hm.getDepth()<=max_depth & hm.getURLS().contains(host1)) {
-                                        int sizea = not_viewed_url.size();
-                                        int sizeb = viewed_url.size();
-                                        boolean aState=false;
-                                        boolean bState=false;
-                                        int i=0;
-                                        int j=0;
-                                            while ((!aState & i<sizea)|(!bState & i<sizeb)){
-                                                if (i<sizea) {
-                                                    if (not_viewed_url.get(i).getURLS().contains(hm.getURLS())) {
-                                                        aState = true;
-                                                    }
-                                                    i++;
-                                                }
-                                                if (j<sizeb){
-                                                    if (viewed_url.get(j).getURLS().contains(hm.getURLS())) {
-                                                        bState = true;
-                                                    }
-                                                    j++;
-                                                }
-                                            }
-                                            if (!aState & !bState) {
-                                                not_viewed_url.add(hm);
-                                            }
-                                        }
-                                    }
-                                } catch (StringIndexOutOfBoundsException e) {
-                                    }
-                        string = reader.readLine();
+    public static void main(String[] args) {
+        new Crawler(new String[]{"http://www.mtuci.ru/","1"});
+    }
+
+    Crawler(String[] args){
+        int max_depth = Integer.parseInt(args[1]);
+        LinkedList<URLDepthPair> not_viewed_urls = new LinkedList<URLDepthPair>();
+        LinkedList<URLDepthPair> viewed_urls = new LinkedList<URLDepthPair>();
+        URLDepthPair first_depthpair = new URLDepthPair(args[0], 0);
+        not_viewed_urls.add(first_depthpair);
+        int j=0;
+        while (not_viewed_urls.size() != 0) {
+            URLDepthPair depthPair = not_viewed_urls.pop();
+            System.out.println("Проверяется ссылка:" + '\t' + depthPair.getURL() + '\n'+
+                    "Ссылок проверено:" + '\t' + ++j);
+            viewed_urls.add(depthPair);
+            int url_depth = depthPair.getDepth();
+            LinkedList<String> linksList = new LinkedList<String>();
+            try{
+                linksList = getAllLinks(depthPair);
+            } catch (IOException e){ }
+            if (url_depth < max_depth) {
+                for (int i=0;i<linksList.size();i++) {
+                    String newURL = linksList.get(i);
+                    if (!(not_viewed_urls.contains(new URLDepthPair(newURL, 0)) | viewed_urls.contains(new URLDepthPair(newURL, 0)))){
+                        not_viewed_urls.add(new URLDepthPair(newURL, newURL.split("/").length-3));
                     }
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
-
-            } catch (MalformedURLException ex) {
-                ex.printStackTrace();
             }
-            soc.close();
-            viewed_url.add(not_viewed_url.getFirst());
-            not_viewed_url.removeFirst();
-            System.out.println("Проверенная cсылка: " + viewed_url.getLast().getURLS());
-            System.out.println("Ссылок проверено: " + viewed_url.size());
         }
-        viewed_url.sort(new Comparator<URLDepthPair>() {
-            @Override
-            public int compare(URLDepthPair o1, URLDepthPair o2) {
-                return o1.getURLS().compareTo(o2.getURLS());
-            }
-        });
-        Set<URLDepthPair> list = new HashSet<URLDepthPair>(viewed_url);
-        viewed_url.clear();
-        viewed_url.addAll(list);
-        System.out.println(viewed_url);
+
+        int i=0;
+        for (URLDepthPair list : viewed_urls) System.out.println(++i + ": " + list.getURL() + " [" + list.getDepth() + "]");
     }
 
-    public static void main(String args[]) throws Exception {
-        new Crawler("nytimes.com");
+    public static LinkedList<String> getAllLinks(URLDepthPair myDepthPair) throws IOException {
+        LinkedList<String> URLs = new LinkedList<String>();
+        Socket sock = new Socket(myDepthPair.getWebHost(), 80);
+        sock.setSoTimeout(3000);
+        PrintWriter myWriter = new PrintWriter(sock.getOutputStream(), true);
+        myWriter.println("GET " + myDepthPair.getDocPath() + " HTTP/1.1");
+        myWriter.println("Host: " + myDepthPair.getWebHost());
+        myWriter.println("Connection: close");
+        myWriter.println();
+        BufferedReader BuffReader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        String line;
+        while (true) {
+            line=BuffReader.readLine();
+            if (line==null){ break; }
+            for (String reta : line.split("href=\"")) {
+                if (line.contains("a href=\"") & reta.startsWith(URL_PREFIX)){
+                    try {
+                        String Link = (reta.substring(0, reta.indexOf("\"")));
+                        URLs.add(Link);
+                    }catch (IndexOutOfBoundsException e){
+                        System.err.println(e);
+                        break;
+                    }
+                }
+            }
+        }
+        sock.close();
+        BuffReader.close();
+        return URLs;
     }
+
 }
--- LAB7 WEB scan/src/URLDepthPair.java
+++ LAB7 WEB scan/src/URLDepthPair.java
@@ -1,19 +1,52 @@
+import java.net.*;
+import java.util.Objects;
+
+
 public class URLDepthPair {
-    int depth;
-    String urls;
 
-    URLDepthPair(String url, int depth) {
-        urls=url;
-        this.depth=depth;
+    private int depth;
+    private String URL;
+
+    public URLDepthPair(String URL, int depth) {
+        this.depth = depth;
+        this.URL = URL;
     }
-    URLDepthPair(String url) {
-        urls=url;
-        this.depth=0;
+
+    public String getURL() { return URL; }
+
+    public int getDepth() { return depth; }
+
+    public String toString() {
+        String stringDepth = Integer.toString(depth);
+        return URL + '\t' + stringDepth;
     }
-    public int getDepth(){return depth;}
 
-    public String getURLS(){return urls;}
+    public String getDocPath() {
+        try {
+            return new URL(URL).getPath();
+        } catch (MalformedURLException e) {
+            return null;
+        }
+    }
+    public String getWebHost() {
+        try {
+            return new URL(URL).getHost();
+        } catch (MalformedURLException e) {
+            return null;
+        }
+    }
 
-    public String toString(){return urls +" "+ depth;}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof URLDepthPair) {
+            URLDepthPair o = (URLDepthPair)obj;
+            return this.URL.equals(o.getURL());
+        }
+        return false;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash();
+    }
 }
